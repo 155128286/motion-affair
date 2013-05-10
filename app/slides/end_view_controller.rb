debaaf424a66dcc990e07cb176927a941078aefb
Yan Li <eyeplum@gmail.com>
2013-05-10 23:27:38 +0800

    finishe p10

--- app/slides/end_view_controller.rb
+++ app/slides/end_view_controller.rb
@@ -1,17 +1,23 @@
 class EndViewController <RViewController
 
-  # TODO : Skull designed by Cameron Pinegar from The Noun Project
-
   def loadView
     super
 
-    q_n_a = RLabel.bold_label_at center_frame_with_size([300, 100]), 'demo'
-    self.view << q_n_a
+    demo = RLabel.bold_label_at center_frame_with_size([300, 100]), 'demo'
+    self.view << demo
 
-    label = RLabel.label_at [[600, 720], [400, 20]], 'Back icon designed by SuperAtic LABS from The Noun Project'
+    skull_copyright = make_label [[600, 700], [400, 20]], 'Skull designed by Cameron Pinegar from The Noun Project'
+    self.view << skull_copyright
+
+    back_copyright = make_label [[600, 720], [400, 20]], 'Back icon designed by SuperAtic LABS from The Noun Project'
+    self.view << back_copyright
+  end
+
+  def make_label(frame, text)
+    label = RLabel.label_at frame, text
     label.font = :small.uifont
     label.textAlignment = NSTextAlignmentRight
-    self.view << label
+    label
   end
 
 end
--- app/slides/p10_view_controller.rb
+++ app/slides/p10_view_controller.rb
@@ -1,8 +1,63 @@
 class P10ViewController <RViewController
 
-  # 1. Obj-C repos -> Cocoapods -> motion-cocoapods -> Rubymotion
-  # 2. Runtime Crash (AddressBook, CoreLocation, UITextField)
-  # 3. Clang vs Rubymotion - ARCLite (PSTCollectionView)
+  def loadView
+    super
+
+    @pit_fall = RImageView.image_view_at center_frame_with_size([512, 512]), 'pitfalls.png'
+    @pit_fall.alpha = 0.0
+    @pit_fall.on_tap {
+      pit_fall_tapped
+    }
+    self.view << @pit_fall
+
+    @cocoapods = make_image_view 'cocoapods.png'
+    self.view << @cocoapods
+
+    @runtime = make_image_view 'runtime_crash_uitextfield.png'
+    self.view << @runtime
+
+    @arc_lite = make_image_view 'arc_lite.png'
+    self.view << @arc_lite
+
+    @content = 0
+  end
+
+  def make_image_view(image)
+    image_view = RImageView.image_view_at center_frame_with_size([1024, 768]), image
+    image_view.alpha = 0.0
+    image_view.on_tap {
+      image_view.close_in lambda {
+        @pit_fall.pop_out
+      }
+    }
+    image_view
+  end
+
+  def pit_fall_tapped
+    @pit_fall.close_in lambda {
+      case @content
+        when 0
+          @cocoapods.pop_out
+        when 1
+          @runtime.pop_out
+        when 2
+          @arc_lite.pop_out
+        else
+          # do nothing
+      end
+
+      if @content < 2
+        @content += 1
+      else
+        @content = 0
+      end
+    }
+  end
+
+  def viewDidAppear(animated)
+    super
+    @pit_fall.pop_out unless @pit_fall.alpha == 1
+  end
 
   def swipe_left
     app_delegate.open 'end'
