7bf9b5c1407865d4bd5f55ffd6499e89ac12839a
Yan Li <eyeplum@gmail.com>
2013-05-11 00:52:45 +0800

    a little fun

--- app/slides/p10_view_controller.rb
+++ app/slides/p10_view_controller.rb
@@ -1,5 +1,11 @@
 class P10ViewController <RViewController
 
+  MOTION_TALK = [
+      'Not my fault!',
+      'Oops...',
+      'Stupid LLVM...'
+  ]
+
   def loadView
     super
 
@@ -9,6 +15,9 @@ class P10ViewController <RViewController
       pit_fall_tapped
     }
     self.view << @pit_fall
+
+    @motion_talk = RLabel.bold_label_at [[78, 355], [356, 50]], '', 32, true
+    @pit_fall << @motion_talk
 
     @cocoapods = make_image_view 'cocoapods.png'
     self.view << @cocoapods
@@ -20,6 +29,11 @@ class P10ViewController <RViewController
     self.view << @arc_lite
 
     @content = 0
+  end
+
+  def viewDidLoad
+    super
+    @pit_fall.pop_out
   end
 
   def make_image_view(image)
@@ -35,13 +49,15 @@ class P10ViewController <RViewController
 
   def pit_fall_tapped
     @pit_fall.close_in lambda {
+      completion = lambda { @motion_talk.text = MOTION_TALK[@content - 1] }
+
       case @content
         when 0
-          @cocoapods.pop_out
+          @cocoapods.pop_out completion
         when 1
-          @runtime.pop_out
+          @runtime.pop_out completion
         when 2
-          @arc_lite.pop_out
+          @arc_lite.pop_out completion
         else
           # do nothing
       end
@@ -52,11 +68,6 @@ class P10ViewController <RViewController
         @content = 0
       end
     }
-  end
-
-  def viewDidAppear(animated)
-    super
-    @pit_fall.pop_out unless @pit_fall.alpha == 1
   end
 
   def swipe_left
