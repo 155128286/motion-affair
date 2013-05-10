762e66e99c82b26e2db0434c1d1a9db89e5a545b
Zeiss <eyeplum@gmail.com>
2013-05-11 01:08:15 +0800

    final tuning

--- app/slides/end_view_controller.rb
+++ app/slides/end_view_controller.rb
@@ -3,8 +3,11 @@ class EndViewController <RViewController
   def loadView
     super
 
-    demo = RLabel.bold_label_at center_frame_with_size([300, 100]), 'demo'
+    demo = RLabel.bold_label_at [[360, 270], [300, 100]], 'demo'
     self.view << demo
+
+    fork_me = RLabel.bold_label_at [[192, 420], [640, 50]], 'github.com/eyeplum/motion-affair', 32
+    self.view << fork_me
 
     skull_copyright = make_label [[600, 700], [400, 20]], 'Skull designed by Cameron Pinegar from The Noun Project'
     self.view << skull_copyright
